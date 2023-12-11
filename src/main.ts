dffcf0a58773cca2ebd6e8e4679b05aa5e8cc29e
AleixAj <aleixauque@gmail.com>
2023-12-11 13:27:42 +0100

    update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,5 @@
 import "./style.css";
 
-/*
-Tienes un grupo de 6 amigos y quieres invitarlos a cenar por tu cumpleaños.
-
-- Solo puedes permitirte invitar a las bebidas, ya que estás un poco ajustado de dinero.
-- Tienes un ticket de cena que cuesta 120 € y en el que ya se incluyen las bebidas por un valor de 18 €.
-- Calcula cuánto tendría que pagar cada comensal para dividir los costos de manera equitativa.
-- Utiliza JavaScript para hacer el cálculo y mostrar el resultado por consola.
-*/
-
 console.log((120 - 18) / 7);            // Lo que tendrá que pagar cada amigo
 console.log(((120 - 18) / 7) + 18);     // Lo que tengo que pagar yo
 
